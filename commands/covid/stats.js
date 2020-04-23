b2c8a5ed6ef65c3c3f68769b39edf2f13b2032b3
Nam <nnhnam6626@gmail.com>
2020-04-23 20:25:52 +0200

    1.3.0
    Use new API

--- commands/covid/stats.js
+++ commands/covid/stats.js
@@ -30,84 +30,88 @@ module.exports = class Stats extends Command {
 
     async run(message, { country }) {
         const messageEmbed = new Discord.MessageEmbed().setColor('#DC143C');
-        await axios.get('https://api.covid19api.com/summary')
-            .then(async (response) => {
-                let data;
-                let countryName;
-                if (country.toLowerCase() === 'global') {
-                    data = response.data.Global;
-                    countryName = 'Global';
-                }
-                else {
-                    const countriesList = [];
-                    for (let i = 0; i < response.data.Countries.length; ++i) {
-                        countriesList.push(response.data.Countries[i].Slug);
-                        countriesList.push(response.data.Countries[i].CountryCode);
+        try {
+            let data;
+            let countryName;
+            let countryCode;
+            if (country.toLowerCase() === 'global') {
+                countryCode = 'global';
+                countryName = 'Global';
+            }
+            else {
+                const response = await axios.get('https://api.thevirustracker.com/free-api?countryTotals=ALL');
+                const countriesList = [];
+                for (const key in response.data.countryitems[0]) {
+                    if (response.data.countryitems[0][key].title && response.data.countryitems[0][key].code) {
+                        countriesList.push(response.data.countryitems[0][key].title);
+                        countriesList.push(response.data.countryitems[0][key].code);
                     }
-                    const fset = FuzzySet(countriesList);
-                    const c = fset.get(country);
-                    // Check if there is any matching country
-                    if (c && c.length > 0) {
-                        for (let i = 0; i < response.data.Countries.length; ++i) {
-                            if (response.data.Countries[i].Slug === c[0][1] || response.data.Countries[i].CountryCode === c[0][1]) {
-                                data = response.data.Countries[i];
-                                countryName = response.data.Countries[i].Country;
-                                break;
-                            }
+                }
+                const fset = FuzzySet(countriesList);
+                const c = fset.get(country);
+                // Check if there is any matching country
+                if (c && c.length > 0) {
+                    for (const key in response.data.countryitems[0]) {
+                        if (response.data.countryitems[0][key].title === c[0][1] || response.data.countryitems[0][key].code === c[0][1]) {
+                            countryCode = response.data.countryitems[0][key].code;
+                            countryName = response.data.countryitems[0][key].title;
+                            data = response.data.countryitems[0][key];
                         }
                     }
-                    // If not set country to Global
-                    else {
-                        data = response.data.Global;
-                        countryName = 'Global';
-                    }
                 }
-
-                const canvasRenderService = new CanvasRenderService(500, 500);
-                const image = await canvasRenderService.renderToBuffer({
-                    type: 'doughnut',
-                    data: {
-                        datasets: [{
-                            data: [data.TotalConfirmed - data.TotalDeaths - data.TotalRecovered, data.TotalDeaths, data.TotalRecovered],
-                            backgroundColor: [
-                                '#f4c63d',
-                                '#d70206',
-                                '#e6d7c1',
-                            ],
-                            borderWidth: 0,
-                        }],
-                        labels: ['Active', 'Deaths', 'Recovered'],
-                    },
-                    options: {
-                        legend: {
-                            labels: {
-                                fontColor: '#988e99',
-                                fontSize: 22,
-                            },
+                // If not set country to Global
+                else {
+                    countryCode = 'global';
+                    countryName = 'Global';
+                }
+            }
+            if (countryCode === 'global') {
+                const response = await axios.get('https://api.thevirustracker.com/free-api?global=stats');
+                data = response.data.results[0];
+            }
+            const canvasRenderService = new CanvasRenderService(500, 500);
+            const image = await canvasRenderService.renderToBuffer({
+                type: 'doughnut',
+                data: {
+                    datasets: [{
+                        data: [data.total_active_cases, data.total_deaths, data.total_recovered],
+                        backgroundColor: [
+                            '#f4c63d',
+                            '#d70206',
+                            '#e6d7c1',
+                        ],
+                        borderWidth: 0,
+                    }],
+                    labels: ['Active', 'Deaths', 'Recovered'],
+                },
+                options: {
+                    legend: {
+                        labels: {
+                            fontColor: '#988e99',
+                            fontSize: 22,
                         },
                     },
-                });
-                const attachment = new Discord.MessageAttachment(image, 'stats.png');
-                messageEmbed
-                    .setAuthor('COVID-19 stats')
-                    .setDescription(`${data.CountryCode ? ':flag_' + data.CountryCode.toLowerCase() + ':' : '🌐'}  ${countryName}`)
-                    .setTitle(`Active: ${data.TotalConfirmed - data.TotalDeaths - data.TotalRecovered}`)
-                    .addFields(
-                        { name: '😷 Confirmed', value: `Total: ${data.TotalConfirmed} \n New: ${data.NewConfirmed}`, inline: true },
-                        { name: '💀 Deaths', value: `Total: ${data.TotalDeaths} \n New: ${data.NewDeaths}`, inline: true },
-                        { name: '💪 Recovered', value: `Total: ${data.TotalRecovered} \n New: ${data.NewRecovered}`, inline: true },
-                    )
-                    .setFooter('Last updated : ')
-                    .attachFiles([attachment])
-                    .setImage('attachment://stats.png')
-                    .setTimestamp(response.data.Date);
-            },
-            )
-            .catch(error => {
-                messageEmbed
-                    .setTitle('Error occured while getting data');
-                console.error(error);
+                },
             });
+            const attachment = new Discord.MessageAttachment(image, 'stats.png');
+            messageEmbed
+                .setAuthor('COVID-19 stats')
+                .setDescription(`${countryCode !== 'global' ? ':flag_' + countryCode.toLowerCase() + ':' : '🌐'}  ${countryName}`)
+                .setTitle(`Active: ${data.total_active_cases}`)
+                .addFields(
+                    { name: '😷 Confirmed', value: `${data.total_cases}`, inline: true },
+                    { name: '💀 Deaths', value: `${data.total_deaths}`, inline: true },
+                    { name: '💪 Recovered', value: `${data.total_recovered}`, inline: true },
+                )
+                .setFooter('')
+                .attachFiles([attachment])
+                .setImage('attachment://stats.png')
+                .setTimestamp();
+        }
+        catch (error) {
+            messageEmbed.setTitle('Error occured while getting data');
+            console.error(error);
+        }
         return message.embed(messageEmbed);
     }
 };
--- commands/covid/top.js
+++ commands/covid/top.js
@@ -25,19 +25,19 @@ module.exports = class Top extends Command {
             ],
         });
         this.compare = {
-            'active': (a, b) => (b.TotalConfirmed - b.TotalDeaths - b.TotalRecovered) - (a.TotalConfirmed - a.TotalDeaths - a.TotalRecovered),
-            'confirmed': (a, b) => (b.TotalConfirmed) - (a.TotalConfirmed),
-            'deaths': (a, b) => (b.TotalDeaths) - (a.TotalDeaths),
-            'recovered': (a, b) => (b.TotalRecovered) - (a.TotalRecovered),
+            'active': (a, b) => (b.total_active_cases) - (a.total_active_cases),
+            'confirmed': (a, b) => (b.total_cases) - (a.total_cases),
+            'deaths': (a, b) => (b.total_deaths) - (a.total_deaths),
+            'recovered': (a, b) => (b.total_recovered) - (a.total_recovered),
         };
     }
 
     getValue(country, type) {
         switch (type) {
-            case 'active': return country.TotalConfirmed - country.TotalDeaths - country.TotalRecovered;
-            case 'confirmed': return country.TotalConfirmed;
-            case 'deaths': return country.TotalDeaths;
-            case 'recovered': return country.TotalRecovered;
+            case 'active': return country.total_active_cases;
+            case 'confirmed': return country.total_cases;
+            case 'deaths': return country.total_deaths;
+            case 'recovered': return country.total_recovered;
         }
     }
 
@@ -45,83 +45,79 @@ module.exports = class Top extends Command {
         if (['active', 'confirmed', 'deaths', 'recovered'].indexOf(type.toLowerCase()) === -1) type = 'active';
         else type = type.toLowerCase();
         const messageEmbed = new Discord.MessageEmbed().setColor('#DC143C');
-
-
-        await axios.get('https://api.covid19api.com/summary')
-            .then(async (response) => {
-                const data = response.data.Countries.sort(this.compare[type]).slice(0, 9);
-                const fields = [];
-                data.forEach((country, index) => {
-                    fields.push({
-                        name: `${index + 1}. :flag_${country.CountryCode.toLowerCase()}: ${country.Country}`,
-                        value: this.getValue(country, type),
-                        inline: true,
-                    });
+        try {
+            const response = await axios.get('https://api.thevirustracker.com/free-api?countryTotals=ALL');
+            const data = Object.values(response.data.countryitems[0]).sort(this.compare[type]).slice(0, 9);
+            const fields = [];
+            data.forEach((country, index) => {
+                fields.push({
+                    name: `${index + 1}. :flag_${country.code.toLowerCase()}: ${country.title}`,
+                    value: this.getValue(country, type),
+                    inline: true,
                 });
-
-                const canvasRenderService = new CanvasRenderService(500, 500);
-                const image = await canvasRenderService.renderToBuffer({
-                    type: 'bar',
-                    data: {
-                        datasets: [{
-                            label: type,
-                            data: data.map(country => this.getValue(country, type)),
-                            backgroundColor: [
-                                '#d70206',
-                                '#db1b1f',
-                                '#df3538',
-                                '#e34e51',
-                                '#e7676a',
-                                '#eb8183',
-                                '#ef9a9b',
-                                '#f3b3b4',
-                                '#f7cccd',
-                                '#fbe6e6',
-                            ],
-                            borderWidth: 0,
-                        }],
-                        labels: data.map(country => country.CountryCode),
-                    },
-                    options: {
-                        legend: {
-                            labels: {
-                                fontColor: '#988e99',
-                                fontSize: 30,
-                            },
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true,
-                                    fontColor: '#988e99',
-                                    fontSize: 18,
-                                },
-                            }],
-                            xAxes: [{
-                                ticks: {
-                                    fontColor: '#988e99',
-                                    fontSize: 18,
-                                },
-                            }],
-                        },
-                    },
-                });
-                const attachment = new Discord.MessageAttachment(image, 'top.png');
-                messageEmbed
-                    .setAuthor('COVID-19')
-                    .setTitle(`📊 Top 9 countries (${type.toLowerCase()})`)
-                    .addFields(fields)
-                    .setFooter('Last updated : ')
-                    .attachFiles([attachment])
-                    .setImage('attachment://top.png')
-                    .setTimestamp(response.data.Date);
-            },
-            )
-            .catch(error => {
-                messageEmbed
-                    .setTitle('Error occured while getting data');
-                console.error(error);
             });
+
+            const canvasRenderService = new CanvasRenderService(500, 500);
+            const image = await canvasRenderService.renderToBuffer({
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        label: type,
+                        data: data.map(country => this.getValue(country, type)),
+                        backgroundColor: [
+                            '#d70206',
+                            '#db1b1f',
+                            '#df3538',
+                            '#e34e51',
+                            '#e7676a',
+                            '#eb8183',
+                            '#ef9a9b',
+                            '#f3b3b4',
+                            '#f7cccd',
+                            '#fbe6e6',
+                        ],
+                        borderWidth: 0,
+                    }],
+                    labels: data.map(country => country.code),
+                },
+                options: {
+                    legend: {
+                        labels: {
+                            fontColor: '#988e99',
+                            fontSize: 30,
+                        },
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                fontColor: '#988e99',
+                                fontSize: 18,
+                            },
+                        }],
+                        xAxes: [{
+                            ticks: {
+                                fontColor: '#988e99',
+                                fontSize: 18,
+                            },
+                        }],
+                    },
+                },
+            });
+            const attachment = new Discord.MessageAttachment(image, 'top.png');
+            messageEmbed
+                .setAuthor('COVID-19')
+                .setTitle(`📊 Top 9 countries (${type.toLowerCase()})`)
+                .addFields(fields)
+                .setFooter('Last updated : ')
+                .attachFiles([attachment])
+                .setImage('attachment://top.png')
+                .setTimestamp(response.data.Date);
+        }
+        catch (error) {
+            messageEmbed.setTitle('Error occured while getting data');
+            console.error(error);
+        }
         return message.embed(messageEmbed);
     }
 };
